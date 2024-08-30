13675646809a14fd63003b0698320fd06c534084
Shivnath Kisku <124551959+Shivkisku@users.noreply.github.com>
2024-08-30 11:14:44 +0530

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Spinner from '../components/Spinner';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <Spinner />
+    </div>
+  );
+}
